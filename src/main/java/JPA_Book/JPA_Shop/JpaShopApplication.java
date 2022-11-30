be3eb783ce6c85d431fc4659d3b4c5492bed89ee
Pavel_Dmr <dlrkdals227@gmail.com>
2022-11-30 10:41:30 +0900

    Initial commit

---
+++ src/main/java/JPA_Book/JPA_Shop/JpaShopApplication.java
@@ -0,0 +1,13 @@
+package JPA_Book.JPA_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShopApplication.class, args);
+	}
+
+}
